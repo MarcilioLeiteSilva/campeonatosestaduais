a1e06096a56e072c6bfbcf8aa3e3f4ef68b8d1df
Marcilio <prolaser2005hotmail.com>
2026-06-29 18:27:02 -0300

    fix: replace escaped newlines in private key string

--- zapscore_sync/index.js
+++ zapscore_sync/index.js
@@ -29,6 +29,9 @@ try {
   }
 
   if (serviceAccount) {
+    if (serviceAccount.private_key) {
+      serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+    }
     admin.initializeApp({
       credential: admin.cert(serviceAccount)
     });
